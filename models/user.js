e341acfe8916b695e1918b1b0e5486a669bea830
Gal Parselani <Parselinho@gmail.com>
2023-04-28 00:04:54 +0300

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,39 @@
+// models/users.js
+
+const { DataTypes } = require('sequelize');
+const { sequelize } = require('./index');
+
+const User = sequelize.define('User', {
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  emailAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+
+module.exports = User;
