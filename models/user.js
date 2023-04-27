4e64b6b5f4356e2726692fc8a0510c8704dd068b
Gal Parselani <Parselinho@gmail.com>
2023-04-28 01:15:32 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,47 +1,50 @@
 'use strict';
 
-// models/users.js
-const Sequelize = require('sequelize');
+// Import DataTypes from sequelize
 const { DataTypes } = require('sequelize');
-const { sequelize } = require('./index');
 
+// Export a function that takes the sequelize instance as an argument
+module.exports = (sequelize) => {
+  // Define the User model using sequelize.define
+  const User = sequelize.define('User', {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        notEmpty: true,
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  });
 
+  // Define the association between User and Course models
+  User.associate = models => {
+    // A User has many Courses, with the foreign key 'userId'
+    User.hasMany(models.Course, { foreignKey: 'userId' });
+  };
 
-const User = sequelize.define('User', {
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-  emailAddress: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      notEmpty: true,
-      isEmail: true,
-    },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      notEmpty: true,
-    },
-  },
-});
-
-User.associate = models => {
-  User.hasMany(models.Course, { foreignKey: 'userId' });
+  // Return the User model
+  return User;
 };
-
-module.exports = User;
