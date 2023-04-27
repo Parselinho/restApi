42aba7bc691f2e4ac0adaabf819630a717cc0792
Gal Parselani <Parselinho@gmail.com>
2023-04-28 01:44:06 +0300

    Update course.js

--- models/course.js
+++ models/course.js
@@ -1,10 +1,14 @@
 'use strict';
 
+// Import Sequelize library
 const Sequelize = require('sequelize');
 
+// Export a function that takes the sequelize instance as an argument
 module.exports = sequelize => {
+  // Define the Course model by extending the Sequelize.Model class
   class Course extends Sequelize.Model {}
 
+  // Initialize the Course model with its attributes and their data types
   Course.init(
     {
       title: {
@@ -37,9 +41,12 @@ module.exports = sequelize => {
     { sequelize }
   );
 
+  // Define the association between the Course and User models
   Course.associate = models => {
+    // A Course belongs to a User, with the foreign key 'userId'
     Course.belongsTo(models.User, { foreignKey: 'userId' });
   };
 
+  // Return the Course model
   return Course;
 };
